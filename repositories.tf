e115728c63494f285e73b382f8d8b334e0012de1
Matt Ashcraft <ashcraft39@gmail.com>
2024-06-23 16:48:14 -0400

    Add branch protection

--- repositories.tf
+++ repositories.tf
@@ -4,3 +4,27 @@ resource "github_repository" "repos" {
   description = each.value.description
   visibility  = each.value.visibility
 }
+
+resource "github_branch_protection_v3" "main" {
+  for_each = var.repositories
+
+  repository     = github_repository.repos[each.key].name
+  pattern        = "main"
+  enforce_admins = true
+
+  required_status_checks {
+    strict   = true
+    contexts = []
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    require_code_owner_reviews      = true
+    required_approving_review_count = 1
+  }
+
+  restrictions {
+    users = []
+    teams = []
+  }
+}
