055abe363b6a66d2485418cb88f6e96ae0686087
Matt Ashcraft <ashcraft39@gmail.com>
2024-06-24 17:42:53 -0400

    Use version 5.0.0

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "4.13.0"
+      version = "5.0.0"
     }
   }
   required_version = "1.5.2"
