f2a2f1a188c2fa89bfc0285f837adf5e936c35d2
Matt Ashcraft <ashcraft39@gmail.com>
2024-06-24 20:41:40 -0400

    Remove github_branch and branch protection

--- repositories.tf
+++ repositories.tf
@@ -6,42 +6,8 @@ resource "github_repository" "repos" {
   auto_init   = true
 }
 
-/*
-resource "github_branch" "main"{
-  for_each    = var.repositories
-  repository  = github_repository.repos[each.key].name
-  branch      = "main"
-}
-*/
-
 resource "github_branch_default" "default"{
   for_each    = var.repositories
   repository  = github_repository.repos[each.key].name
   branch      = "main"
 }
-
-/*
-resource "github_branch_protection_v3" "main" {
-  for_each = var.repositories
-
-  repository     = github_repository.repos[each.key].name
-  branch         = "main"
-  enforce_admins = true
-
-  required_status_checks {
-    strict   = true
-    contexts = []
-  }
-
-  required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    require_code_owner_reviews      = true
-    required_approving_review_count = 1
-  }
-
-  restrictions {
-    users = []
-    teams = []
-  }
-}
-*/
